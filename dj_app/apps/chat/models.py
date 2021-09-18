b5f02179455a7c1c383bc4cbe7724bc2865ffcaa
JangasCodingplace <janisgoesser92@gmail.com>
2021-09-18 16:46:45 +0200

    Init Chat application

--- dj_app/apps/chat/models.py
+++ dj_app/apps/chat/models.py
@@ -1,3 +1,40 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Thread(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        editable=False,
+        default=uuid.uuid1
+    )
+    user = models.JSONField()
+    timestamp = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.id
+
+
+class Message(models.Model):
+    thread = models.ForeignKey(
+        to=Thread,
+        on_delete=models.CASCADE,
+        related_name="messages"
+    )
+    user = models.JSONField()
+    timestamp = models.DateTimeField(
+        auto_now_add=True
+    )
+    body = models.TextField()
+    send_by_user = models.BooleanField(
+        default=True,
+        help_text="If False the message was sent from Slack"
+    )
+
+    class Meta:
+        ordering = ('-timestamp', )
+
+    def __str__(self):
+        return f"Msg for Thread {self.thread.id}"
