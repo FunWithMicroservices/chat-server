acd099c94a141a429f80caccfa45a1c49ebd57f5
JangasCodingplace <janisgoesser92@gmail.com>
2021-09-19 15:18:28 +0200

    Update Slack Receiver

--- dj_app/apps/slack_integration/views.py
+++ dj_app/apps/slack_integration/views.py
@@ -1,11 +1,35 @@
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
+from apps.chat.models import Message
+from .models import SlackThread
 
 
 class SlackAPI(CreateAPIView):
     def post(self, request, *args, **kwargs):
-        challenge = request.data["challenge"]
-        return Response({"challenge": challenge})
+        if request.data["type"] == "url_verification":
+            return Response({"challenge": request.data["challenge"]})
+
+        event_id = request.data["event_id"]
+        event = request.data["event"]
+        if event["type"] != "message":
+            return Response({"challenge": event_id})
+
+        if "thread_ts" not in event:
+            return Response({"challenge": event_id})
+
+        try:
+            thread = SlackThread.objects.get(thread_ts=event["thread_ts"])
+        except SlackThread.DoesNotExist:
+            return Response({"challenge": event_id})
+
+        Message.objects.create(
+            thread=thread.source_thread,
+            user={"username": "slack"},
+            body=event["text"],
+            send_by_user=False
+        )
+
+        return Response({"challenge": event_id})
 
 
 slack_api = SlackAPI.as_view()
