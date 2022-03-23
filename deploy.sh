721d3ef199b7aab2acd29b452582914cc0700b42
JangasCodingplace <janisgoesser92@gmail.com>
2022-03-23 21:57:05 +0100

    Add Kafka Setup and smaller bugfixes

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-while getopts s:t:a: flag
+while getopts s:t:a:k: flag
 do
     case "${flag}" in
+        a) APPLICATION=${OPTARG};;
         s) SECRET_KEY=${OPTARG};;
         t) SLACK_BOT_TOKEN=${OPTARG};;
-        a) AZ_SB_CONN_STR=${OPTARG};;
         k) KAFKA_HOST=${OPTARG};;
     esac
 done
@@ -14,7 +14,6 @@ git pull origin master
 
 export DJ_SECRET_KEY=$SECRET_KEY
 export SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN
-export AZ_SB_CONN_STR=$AZ_SB_CONN_STR
 export KAFKA_HOST=$KAFKA_HOST
 
-docker-compose up --build -d django
+docker-compose up --build -d $APPLICATION
