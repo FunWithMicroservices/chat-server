b802923bb09e28206d29505d2bf244cd4f0a6ba9
JangasCodingplace <janisgoesser92@gmail.com>
2022-03-25 14:36:46 +0100

    update lang detect

--- dj_app/apps/slack_integration/signals.py
+++ dj_app/apps/slack_integration/signals.py
@@ -4,10 +4,14 @@ from django.db.models.signals import post_save
 from apps.chat.models import Message
 from .slack_integration import lead_message_to_slack
 
+from langdetect import detect
+
 
 @receiver(post_save, sender=Message)
 def send_message_to_slack(created, instance, *args, **kwargs):
     if created:
         if not instance.send_by_user:
+            return
+        if detect(instance.body) != 'en':
             return
         lead_message_to_slack(instance)
