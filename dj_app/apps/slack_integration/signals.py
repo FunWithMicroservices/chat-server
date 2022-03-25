429490cc99a6907b0e479e644ab3cc3db88dcaf9
JangasCodingplace <janisgoesser92@gmail.com>
2022-03-25 14:45:45 +0100

    improve logging

--- dj_app/apps/slack_integration/signals.py
+++ dj_app/apps/slack_integration/signals.py
@@ -1,11 +1,15 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
+import logging
+
 from apps.chat.models import Message
 from .slack_integration import lead_message_to_slack
 
 from langdetect import detect
 
+
+logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=Message)
 def send_message_to_slack(created, instance, *args, **kwargs):
@@ -13,5 +17,7 @@ def send_message_to_slack(created, instance, *args, **kwargs):
         if not instance.send_by_user:
             return
         if detect(instance.body) != 'en':
+            logger.info(f"THROW AWAY THAT SHT {instance.body}")
             return
+        logger.info(f"SEND MSG {instance.body}")
         lead_message_to_slack(instance)
